9d43965a98090c223c1eaa466b02b53db2f6a060
antciccone <aciccone@bu.edu>
2016-10-25 10:47:02 -0600

    adding model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Category do
       expect(cateogry).to  be_invalid
     end
 
-    scenario "user can not create the same "
+    scenario "user can not create the same category name" do
+      category_one = Category.create(name: "Tech")
+      category_two = Category.create(name: "Tech")
+
+      expect(category_one).to be_valid
+      expect(category_two).to be_invalid
+    end
   end
 end
