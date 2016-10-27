fc6fcb29a384c33cb46cabe48de4ec55acec9c4f
antciccone <aciccone@bu.edu>
2016-10-27 15:57:59 -0600

    adding functionality to admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,40 +4,10 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def new
-    @category = Category.new
-  end
-
   def show
     @user = User.find(session[:user_id]) if !session[:user_id].nil?
     @category = Category.find(params[:id])
     @ideas = @category.ideas
-  end
-
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-    redirect_to  category_path(@category)
-    else
-    render :new
-    end
-  end
-
-  def edit
-    @category = Category.find(params[:id])
-  end
-
-  def update
-    @category = Category.find(params[:id])
-    @category.update(category_params)
-    redirect_to category_path(@category)
-  end
-
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
-    redirect_to categories_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :categories do
+  resources :categories, only: [:index, :show] do
     resources :ideas, only:  [:show, :edit, :update]
   end
 
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   resources :images
   resources :users
 
-  get  '/login',  to: 'sessions#new'
-  post '/login',  to: 'sessions#create'
+  get     '/login',  to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
 
   namespace :admin do
